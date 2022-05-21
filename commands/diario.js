b0f0750400e0ddb07d6c499c90e6a2e8765f6bcf
Virgulas <bruno.a.wep@gmail.com>
2022-05-21 10:58:44 -0300

    Behavior history added

--- commands/diario.js
+++ commands/diario.js
@@ -1,98 +1,126 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const { channelIds } = require('./config.json');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const { channelIds, roleIds } = require("../config.json");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('diario')
-		.setDescription('Divulgue um acontecimento!!')
-        .addStringOption(option => option.setName('título').setDescription('Escreva seu título!')
-        .setRequired(true))
-        .addStringOption(option => option.setName('descrição').setDescription('Descreva sua notícia.')
-        .setRequired(true))
-        .addStringOption(option => option.setName('imagem').setDescription('Url de uma imagem.')
-        .setRequired(true))
-        .addStringOption(option => option.setName('autor').setDescription('Declare o autor do conteúdo.'))
-        .addStringOption(option => option.setName('subtitulo').setDescription('Adicione um subtitulo.'))
-        .addStringOption(option => option.setName('subdescrição').setDescription('Adicione uma descrição ao subtitulo.'))
-        .addStringOption(option => option.setName('fonte').setDescription('Adicione uma fonte a notícia.'))
-        .addStringOption(option => option.setName('cor').setDescription('Escolha uma cor para a exibição.')),
-	async execute(interaction) {
-        const action = await interaction;
-        try {
-         let url = new URL(action.options.getString('imagem'));
-         let autor = '';
-         if(action.options.getString('autor')) {
-                 autor = " - Autor: " + action.options.getString('autor');
-         }
+  data: new SlashCommandBuilder()
+    .setName("diario")
+    .setDescription("Divulgue um acontecimento!!")
+    .addStringOption((option) =>
+      option
+        .setName("título")
+        .setDescription("Escreva seu título!")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("descrição")
+        .setDescription("Descreva sua notícia.")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("imagem")
+        .setDescription("Url de uma imagem.")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName("autor").setDescription("Declare o autor do conteúdo.")
+    )
+    .addStringOption((option) =>
+      option.setName("subtitulo").setDescription("Adicione um subtitulo.")
+    )
+    .addStringOption((option) =>
+      option
+        .setName("subdescrição")
+        .setDescription("Adicione uma descrição ao subtitulo.")
+    )
+    .addStringOption((option) =>
+      option.setName("fonte").setDescription("Adicione uma fonte a notícia.")
+    )
+    .addStringOption((option) =>
+      option.setName("cor").setDescription("Escolha uma cor para a exibição.")
+    ),
+  async execute(interaction) {
+    const action = await interaction;
+    try {
+      let url = new URL(action.options.getString("imagem"));
+      let autor = "";
+      if (action.options.getString("autor")) {
+        autor = " - Autor: " + action.options.getString("autor");
+      }
 
-         const exampleEmbed = new MessageEmbed()
-	.setColor('#b80000')
-	.setTitle(action.options.getString('título'))
-	.setDescription(action.options.getString('descrição'))
-        .setThumbnail('https://cdn.discordapp.com/attachments/844699066930954302/968664576013004861/Tragi.png')
-        .setImage(action.options.getString('imagem'))
+      const exampleEmbed = new MessageEmbed()
+        .setColor("#b80000")
+        .setTitle(action.options.getString("título"))
+        .setDescription(action.options.getString("descrição"))
+        .setThumbnail(
+          "https://cdn.discordapp.com/attachments/844699066930954302/968664576013004861/Tragi.png"
+        )
+        .setImage(action.options.getString("imagem"))
         .setTimestamp()
-        .setFooter({ text: "Publicado por: " + action.user.username + autor, iconURL: action.user.avatarURL()});
+        .setFooter({
+          text: "Publicado por: " + action.user.username + autor,
+          iconURL: action.user.avatarURL(),
+        });
 
-        let subtitulo = action.options.getString('subtitulo'), subdescrição =  action.options.getString('subdescrição');
-        let fonte = action.options.getString('fonte'), cor = action.options.getString('cor');
+      let subtitulo = action.options.getString("subtitulo"),
+        subdescrição = action.options.getString("subdescrição");
+      let fonte = action.options.getString("fonte"),
+        cor = action.options.getString("cor");
 
-        if(fonte) {
-           let url2 = new URL(fonte);
-           exampleEmbed.setURL(fonte);
+      if (fonte) {
+        let url2 = new URL(fonte);
+        exampleEmbed.setURL(fonte);
+      }
+
+      if (cor) {
+        let cores = {
+          vermelho: "#ed0000",
+          laranja: "#ff8000",
+          amarelo: "#fff200",
+          verde: "#48ff00",
+          azul: "#005eff",
+          roxo: "#a600ed",
+          rosa: "#ff00f7",
+          branco: "#ffffff",
+          preto: "#000000",
+        };
+        if (cores[cor]) {
+          exampleEmbed.setColor(cores[cor]);
         }
+      }
 
-        if(cor) {
-           let cores = {
-                   vermelho: '#ed0000', 
-                   laranja: '#ff8000', 
-                   amarelo: '#fff200', 
-                   verde: '#48ff00',
-                   azul: '#005eff',
-                   roxo: '#a600ed',
-                   rosa: '#ff00f7',
-                   branco: '#ffffff',
-                   preto: '#000000'
-        }
-           if(cores[cor]) {
-                   exampleEmbed.setColor(cores[cor]);
-           }
+      if (subtitulo) {
+        let field = { name: subtitulo, value: "-" };
+        if (subdescrição) field["value"] = subdescrição;
+        exampleEmbed.addFields(field);
+      }
 
-        }
-
-        if(subtitulo) {
-                let field = { name: subtitulo, value: '-'};
-                if(subdescrição) field['value'] = subdescrição;
-                exampleEmbed.addFields(
-                        field
-                )
-        }
-
-        if(interaction.member.roles.cache.get(channelIds['index'])) {
-                action
-                .client
-                .channels
-                .cache
-                .find(channel => channel.id == channelIds['index'])
-                .send({ embeds: [exampleEmbed] })
-                action.reply({ content: 'Sua notícia foi publicada!', ephemeral: false });
-                action.deleteReply();	
-	}
-        else {
-                action
-                .client
-                .channels
-                .cache
-                .find(channel => channel.id == channelIds['index'])
-                .send({ embeds: [exampleEmbed] })
-                action.reply({ content: 'Sua notícia foi publicada!', ephemeral: false });
-                action.deleteReply();	
-        }
-
-        } catch (error) {
-        action.reply({ content: 'URL inválido. Tente novamente!', ephemeral: true });
-        }
-        
-},
+      if (interaction.member.roles.cache.get(roleIds[0])) {
+        action.client.channels.cache
+          .find((channel) => channel.id == channelIds[0])
+          .send({ embeds: [exampleEmbed] });
+        action.reply({
+          content: "Sua notícia foi publicada!",
+          ephemeral: false,
+        });
+        action.deleteReply();
+      } else {
+        action.client.channels.cache
+          .find((channel) => channel.id == channelIds[0])
+          .send({ embeds: [exampleEmbed] });
+        action.reply({
+          content: "Sua notícia foi publicada!",
+          ephemeral: false,
+        });
+        action.deleteReply();
+      }
+    } catch (error) {
+      action.reply({
+        content: "URL inválido. Tente novamente!",
+        ephemeral: true,
+      });
+    }
+  },
 };
--- index.js
+++ index.js
@@ -1,55 +1,166 @@
-const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const { token, channelIds } = require('./config.json');
+const { remove } = require("confusables");
+const fs = require("node:fs");
+const { Client, Collection, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const { token, channelIds } = require("./config.json");
+const { users } = require("./dbSim.json");
 
 //To see this bot running, you need a config.json with the bot token, clientId, guildId and the required channelId's.
-  
+
 client.commands = new Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+let badWords = [
+  "baitola",
+  "bicha",
+  "boceta",
+  "buceta",
+  "bucetão",
+  "cuzão",
+  "fuder",
+  "caralho",
+  "cacete",
+  "porra",
+  "pênis",
+  "transar",
+  "foder",
+  "fodendo",
+  "boquete",
+  "vsf",
+  "fdp",
+  "xereca",
+  "xana",
+  "vagina",
+  "cu",
+  "cuzinho",
+  "bucetinha",
+  "corno",
+  "corna",
+  "cuzuda",
+  "fode",
+  "fodida",
+  "fudido",
+  "fudida",
+  "fodido",
+  "pinto",
+  "pintão",
+  "roludo",
+  "roluda",
+  "sacudo",
+  "piroca",
+  "punheta",
+  "punhetão",
+  "masturbação",
+  "puta",
+  "puto",
+  "siririca",
+  "vagabunda",
+  "vagabundo",
+  "xota",
+  "xoxota",
+  "xaninha",
+  "arrombado",
+  "boqueteiro",
+  "cabaço",
+  "fimosento",
+  "demente",
+  "cocozento",
+  "vadia",
+  "vai a merda",
+  "va a merda",
+  "xavasca",
+  "boiola",
+  "gozada",
+  "gozar",
+  "gozei",
+  "esperma",
+  "masturbar",
+  "masturba",
+  "masturbe",
+  "masturbe-se",
+];
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 //Tells if the bot is on.
 
-client.on('ready', async () => {
-	console.log('on')
-  });
-
-  //Allows only links and attachments in the specified channel.
-
-client.on('messageCreate',  async msg => {
-	if(msg.channelId == channelIds['index']) { 
-		let rg = /(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/;
-		if(!msg.attachments.firstKey()&&!msg.content.match(rg)){
-			setTimeout(() => msg.delete(), 1000)
-		}
-	}
-  });
-
-  //Commands interaction interface. It reads every interaction and if the interaction is listed, It executes
-
-client.on('interactionCreate', async interaction => {
-
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-	
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+client.on("ready", async () => {
+  console.log("on");
 });
 
-  client.login(token);
+//Allows only links and attachments in the specified channel.
 
+client.on("messageCreate", async (msg) => {
+  if (msg.author.bot) return;
+  if (msg.channelId == channelIds[0]) {
+    let rg =
+      /(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/;
+    if (!msg.attachments.firstKey() && !msg.content.match(rg)) {
+      setTimeout(() => msg.delete(), 1000);
+    }
+  }
+
+  let user = users[msg.author.id];
+
+  if (!user) {
+    users[msg.author.id] = {
+      id: msg.author.id,
+      name: `${msg.author.username}`,
+      img: msg.author.avatarURL(),
+      badWords: {},
+    };
+
+    user = users[msg.author.id];
+  }
+
+  let check = msg.content;
+  for (word of badWords) {
+    let rgx = new RegExp("\b?" + remove(word) + "\b?", "g");
+    let bol = remove(check).match(rgx);
+    if (bol) {
+      msg.client.channels.cache
+        .find((ch) => ch.id == "844699066930954303")
+        .send(
+          `Atenção! Um usuário disse uma palavra proibida: \n Nome: ${msg.author.username} \n Id: ${msg.author.id} \n Mensagem: ${msg.content} \n Link: ${msg.url}`
+        );
+      if (!user.badWords[word]) {
+        user.badWords[word] = 1;
+      } else {
+        user.badWords[word]++;
+      }
+      console.log(user);
+    }
+  }
+});
+
+//Commands interaction interface. It reads every interaction and if the interaction is listed, It executes
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(token);
